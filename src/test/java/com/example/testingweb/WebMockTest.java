a3e3b8fdfbd4f1b720539750789d5a11164576d6
Sebastian Pizarro <sebastian.pizarro@tecsystems.cl>
2024-05-11 00:51:59 -0400

    Update Tests

--- src/test/java/com/example/testingweb/WebMockTest.java
+++ src/test/java/com/example/testingweb/WebMockTest.java
@@ -25,8 +25,8 @@ public class WebMockTest {
 
 	@Test
 	public void greetingShouldReturnMessageFromService() throws Exception {
-		when(service.greet()).thenReturn("Hola, soy Sebastian Pizarro.");
+		when(service.greet()).thenReturn("Hello, Mock");
 		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hola, soy Sebastian Pizarro.")));
+				.andExpect(content().string(containsString("Hello, Mock")));
 	}
 }
